65e752890e92ff59155d204eaba77ab60502ed9b
Andrew <7147407+ha-noh@users.noreply.github.com>
2019-11-05 16:54:41 -0500

    chrome.downloads implementation
    +extension now downloads a text file, NowPlayingExport.txt, every time a tab updates

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ const NowPlayingTitleExport = (function() {
 
 	//choose tab, click action to flag it for use with this extension
 	let selectedTab;
-	let nowPlaying;
 	let flag = false;
 	//listen for updates to flagged tab
 	chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
@@ -10,30 +9,17 @@ const NowPlayingTitleExport = (function() {
 
 		//ignore site-specific, non-informative titles
 		if(info.title == 'Your stream on SoundCloud') return;
-		getFilePermission(info.title);
+		writeToFile(info.title);
 	});
 
-
-	//access FileSystem-API
-	function getFilePermission(title) {
-		nowPlaying = title;
-		navigator.webkitPersistentStorage.requestQuota(75, function() {
-			window.webkitRequestFileSystem(window.PERSISTENT, 75, writeToFile, function() {
-				if(!flag) {
-					alert('could not write ' + title + ' to file.');
-					flag = true;
-				}
-			});
+	function writeToFile(title) {
+		const blob = new Blob([title], {type: 'text/plain'});
+		const url = URL.createObjectURL(blob);
+		chrome.downloads.download({
+			url: url,
+			filename: 'NowPlayingExport.txt',
+			conflictAction: 'overwrite'
 		});
-	}
-
-	//write page title to new file
-	function writeToFile(localstorage) {
-		localstorage.root.getFile('NowPlayingExport.txt', {create: true}, function(file) {
-			file.createWriter(function(content) {
-				const blob = new Blob([nowPlaying], {type: 'text/plain'});
-				content.write(blob);
-			});
-		});
-	}
+		//revokeObjectURL() causes download to fail
+	}	
 } ());
