5b10e1e8e5cfd0579d479a24aec65fe2f33c73ba
Andrew <7147407+ha-noh@users.noreply.github.com>
2019-11-05 01:14:38 -0500

    FileSystem method
    + implementation attempt that uses FileSystem API to write the song title to file (Chrome doesn't seem to do anything with it)

--- background.js
+++ background.js
@@ -2,40 +2,38 @@ const NowPlayingTitleExport = (function() {
 
 	//choose tab, click action to flag it for use with this extension
 	let selectedTab;
+	let nowPlaying;
 	let flag = false;
 	//listen for updates to flagged tab
-	chrome.tabs.onUpdated.addListener( function(tabId, info, tab) {
+	chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
 		// if(tabId !== selectedTab) return false;
 
 		//ignore site-specific, non-informative titles
 		if(info.title == 'Your stream on SoundCloud') return;
-		if(!flag) write_str_to_window(info.title);
+		getFilePermission(info.title);
 	});
 
 
-	//write page title to new file
-	function write_str_to_window(str) {
-		let title = 'Now Playing Title Export';
-
-		flag = true;
-		chrome.windows.create({url: 'test.html'});
-		//open a new Now Playing tab if it isn't already, else set w to the window object of the open tab
-		//callback function runs even when query finds no matches 
-		chrome.tabs.query({title}, function(tabs){
-			if(tabs.length) {
-
-			} 
-
-			else {
-
-			} 
+	//access FileSystem-API
+	function getFilePermission(title) {
+		nowPlaying = title;
+		navigator.webkitPersistentStorage.requestQuota(75, function() {
+			window.webkitRequestFileSystem(window.PERSISTENT, 75, writeToFile, function() {
+				if(!flag) {
+					alert('could not write ' + title + ' to file.');
+					flag = true;
+				}
+			});
 		});
+	}
 
-		
-		// w.document.write(`<html>
-		// 	<head>
-		// 	<title>${title}</title>
-		// 	</head>
-		// 	</html>`);
+	//write page title to new file
+	function writeToFile(localstorage) {
+		localstorage.root.getFile('NowPlayingExport.txt', {create: true}, function(file) {
+			file.createWriter(function(content) {
+				const blob = new Blob([nowPlaying], {type: 'text/plain'});
+				content.write(blob);
+			});
+		});
 	}
 } ());
