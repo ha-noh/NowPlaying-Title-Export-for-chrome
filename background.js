12fb27da9d8b8831b74e778e91608f436c5de1b4
Andrew <7147407+ha-noh@users.noreply.github.com>
2019-11-06 13:59:57 -0500

    download history changes
    + Extension now turns off  the chrome download shelf while active, and removes the text file download from history upon completion. This should reduce overall visual clutter
    
    + the extension now (should) programatically remove/deallocate url objects and listeners

--- background.js
+++ background.js
@@ -1,22 +1,27 @@
 const NowPlayingTitleExport = (function() {
 	let currentTitle = '';
 	let extensionToggle = false;
+	
 
 	chrome.browserAction.onClicked.addListener(function() {
 		extensionToggle = !extensionToggle;
 		if(extensionToggle) {
 			//programatically change browser action icons here
 			alert('Now Playing export enabled');
+			chrome.downloads.setShelfEnabled(false);
 		}
 
 		else {
 			alert('Now Playing export disabled');
+			//re-enable chrome download shelf when the extension is not active
+			chrome.downloads.setShelfEnabled(true);
 		}
 	});
 
 	//listen for updates to audible tabs
 	chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
 		if(!extensionToggle) return;
+
 		//site-specific titles to ignore
 		const blacklist = /(^Spotify\s)|(\son\sSoundCloud)|(^YouTube$)/;
 		if(tab.title.search(blacklist) >= 0) return;
@@ -29,14 +34,26 @@ const NowPlayingTitleExport = (function() {
 	function writeToFile(title) {
 		const blob = new Blob([title], {type: 'text/plain'});
 		const url = URL.createObjectURL(blob);
+		let currentId;
+
 		chrome.downloads.download({
 			url: url,
 			filename: 'NowPlayingExport.txt',
 			conflictAction: 'overwrite',
 			saveAs: false
-		});
+		}, id => currentId = id);
 
-		//add listener for download completion to deallocate url
+		chrome.downloads.onChanged.addListener(onChange);
+
+		function onChange(downloadDelta) {
+			//if download complete or interrupted
+			if(downloadDelta.id === currentId && downloadDelta.state && downloadDelta.state !== 'in_progress') {
+				URL.revokeObjectURL(url);
+				chrome.downloads.onChanged.removeListener(onChange);
+				//remove download from history to prevent clutter
+				chrome.downloads.erase({id: downloadDelta.id}, function(){});
+			}
+		}
 
 		//store title
 		currentTitle = title;
